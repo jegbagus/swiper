895a4de239231dada4874af0926fcac64e201a26
Agung Iswara <agung@jegtheme.com>
2022-01-11 17:22:48 +0800

    Fix issue on navigation

--- dist/swiper.esm.js
+++ dist/swiper.esm.js
@@ -3,11 +3,11 @@
  * Most modern mobile touch slider and framework with hardware accelerated transitions
  * https://swiperjs.com
  *
- * Copyright 2014-2021 Vladimir Kharlampidi
+ * Copyright 2014-2022 Vladimir Kharlampidi
  *
  * Released under the MIT License
  *
- * Released on: December 29, 2021
+ * Released on: January 11, 2022
  */
 
 export { default as Swiper, default } from './core/core.js';
